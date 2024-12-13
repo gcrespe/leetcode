e7fd9012ab5eac040817276003bd42377799919f
Giuliano Crespe <gcrespe3@hotmail.com>
2024-12-12 22:22:59 -0300

    [JAVA] Implemented reverse linked list II

---
+++ java/92-reverse-linked-list-ii.java
@@ -0,0 +1,26 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> rs = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while (!stack.empty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            rs.add(root.val);
+            root = root.right;
+        }
+        return rs;
+    }
+}
