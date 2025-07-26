469e311f244c695434154792c7cd4b14fdbe23dd
Giuliano Crespe <gcrespe3@hotmail.com>
2025-07-25 23:13:47 -0300

    Implemented 3487

---
+++ java/3487-max-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+       public int maxSum(int[] nums) {
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+        
+        int maxSum = 0;
+        boolean hasPositive = false;
+        int maxSingle = Integer.MIN_VALUE;
+        
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            int value = entry.getKey();
+            int freq = entry.getValue();
+            
+            maxSingle = Math.max(maxSingle, value);
+            
+            if (value > 0) {
+                maxSum += value;
+                hasPositive = true;
+            }
+        }
+        
+        return hasPositive ? maxSum : maxSingle;
+    }
+}
