1d557cfee2d7c49c84b940e8bdf044abf3de65ff
Giuliano <gcrespe3@hotmail.com>
2026-02-04 20:38:14 +0000

    merge two sorted lists

---
+++ java/MergeTwoSortedLists.java
@@ -0,0 +1,27 @@
+import utils.ListNode;
+
+public class MergeTwoSortedLists {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        ListNode head = new ListNode(0);
+        ListNode current = head;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        current.next = (list1 != null) ? list1 : list2;
+
+        return head.next;
+    }
+}
