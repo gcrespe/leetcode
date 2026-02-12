e6b2b4314e9a5511a0bc2bf1e5499c8e82da30c5
Giuliano <gcrespe3@hotmail.com>
2026-02-12 20:47:55 +0000

    impl balanced binary tree

---
+++ java/BalancedBinaryTree.java
@@ -0,0 +1,19 @@
+import utils.TreeNode;
+
+public class BalancedBinaryTree {
+
+    public boolean isBalanced(TreeNode root) {
+        return checkHeight(root) != -1;
+    }
+
+    public int checkHeight(TreeNode root){
+        if(root == null) return 0;
+
+        int left = checkHeight(root.left);
+        int right = checkHeight(root.right);
+        if (left == -1 || right == -1 || Math.abs(left - right) > 1) return -1;
+
+        return Math.max(left, right) + 1;
+    }
+
+}
